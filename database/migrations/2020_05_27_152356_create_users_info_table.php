aed5320239d542f6c24c3bef6f9dd18f7b7084a8
frank <59645305+frapgn@users.noreply.github.com>
2020-05-27 18:34:42 +0200

    new laravel project

---
+++ database/migrations/2020_05_27_152356_create_users_info_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_info', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->text('bio');
+            $table->string('linkedin');
+            $table->string('twitter');
+            $table->string('github');
+            $table->text('avatar_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_info');
+    }
+}
