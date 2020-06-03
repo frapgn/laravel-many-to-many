8de1256efbf0fed69c03399fa6b8f3c4cecab4d8
frank <59645305+frapgn@users.noreply.github.com>
2020-06-03 20:43:12 +0200

    ordinata la index

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -4,20 +4,32 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Author</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($pages as $page)
+                <table class="table">
+                    <thead>
                         <tr>
-                            <dt>{{$page->title}}</dt>
-                            <dt>{{$page->user->name}}</dt>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th colspan="3">Actions</th>
                         </tr>
-                    @endforeach
-                </tbody>
+                    </thead>
+                    <tbody>
+                        @foreach ($pages as $page)
+                            <tr>
+                                <td>{{$page->title}}</td>
+                                <td>{{$page->user->name}}</td>
+                                <td><a href="{{route('admin.pages.show', $page->id)}}">View</a></td>
+                                <td><a href="{{route('admin.pages.edit', $page->id)}}">Edit</a></td>
+                                <td>
+                                    <form action="{{route('admin.pages.destroy', $page->id)}}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input type="submit" value="Delete">
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
